4b2ba4e9999ab7afec5c888f13cbb98cda75e227
shenweichen <last.fantasy@qq.com>
2016-05-17 01:30:12 +0800

    三维测试

--- biyesheji1/OpenGl.cpp
+++ biyesheji1/OpenGl.cpp
@@ -7,6 +7,8 @@
 using namespace std;
 static GLfloat xspin = 0.0;
 static GLfloat yspin = 0.0;
+static GLfloat zspin = -4.0f;
+static GLfloat edge = 8.0f;
 double dLastX=0.0,dLastY=0.0;
 int ithpath=0;
 int nRange,nStartPoint,nEndPoint;
@@ -23,37 +25,46 @@ void init(void){
 
 void display(){
 		 double tempx,tempy;
-	tempx=(double)xy[nStartPoint].x;
+		tempx=(double)xy[nStartPoint].x;
 	  tempy=(double)xy[nStartPoint].y;
      glClear(GL_COLOR_BUFFER_BIT);
      glPushMatrix();
 	 if(initIsTrue){
-		 glTranslatef(xspin,yspin,0.0);
+		 glTranslatef(xspin,yspin,zspin);
 		initIsTrue=false;}
 	 else
-	 glTranslatef(xspin-(double)xy[nStartPoint].x,yspin-(double)xy[nStartPoint].y,0.0);
-     glColor3f(0.0,0.0,1.0);
-     glBegin(GL_POLYGON);
-        
-    for(int i=0; i<100; ++i)  
-        glVertex2f(R*cos(2*Pi/100*i)+tempx, R*sin(2*Pi/100*i)+tempy);  
-	glEnd();   
-
-	
+	 glTranslatef(xspin-(double)xy[nStartPoint].x,yspin-(double)xy[nStartPoint].y,0);
+    
+	 glColor3f(0.0,0.0,1.0);		
+	 glTranslatef(tempx-2,tempy-2,zspin);
+	 glutWireSphere(0.2,20,16);
      glPopMatrix();
-	       glColor3f(1.0,0.0,0.0);
+
+	  glColor3f(1.0,0.0,0.0);
 	  for(int i=0;i<nObstacleNum;i++){
 		double ob_x=xy[nObstacleID[i]].x;
-		double ob_y=xy[nObstacleID[i]].y;
-	 glBegin(GL_POLYGON);
-    for(int i=0; i<100; ++i)  
-        glVertex2f(R*cos(2*Pi/100*i)+ob_x, R*sin(2*Pi/100*i)+ob_y);  
-	glEnd();   
+		double ob_y=xy[nObstacleID[i]].y;			
+		//-----------------
+		   glPushMatrix();
+		  glTranslatef(ob_x-2,ob_y-2,zspin);
+		 glutWireSphere(0.2,10,10);
+		 glPopMatrix();
+		 //-------------------
 	 }
+
+
 	  glColor3f(0.0,1.0,0.0);
 	  tempx=(double)xy[nEndPoint].x;
 	  tempy=(double)xy[nEndPoint].y;
-	  glRectf(tempx-0.3,tempy-0.3,tempx+0.3,tempy+0.3);
+	  //glRectf(tempx-0.3,tempy-0.3,tempx+0.3,tempy+0.3);
+	     glPushMatrix();
+		
+		  glTranslatef(tempx-2,tempy-2,zspin);
+		 glutSolidSphere(0.2,15,15);
+		 glPopMatrix();
+
+	
+	  glutWireCube(edge);
      glutSwapBuffers();
 
 }
@@ -81,9 +92,13 @@ void reshape(int w,int h){
      glViewport(0,0,(GLsizei)w,(GLsizei)h);
      glMatrixMode(GL_PROJECTION);
      glLoadIdentity();
-     glOrtho(0.0,(double)nRange,0.0,(double)nRange,-1.0,1.0);
+	 glFrustum(-1, 1, -1, 1, 1, 20);  
+	// gluPerspective(60.0,(GLfloat)w/(GLfloat)h,100.0,1000.0);
+    // glOrtho(0.0,(double)nRange,0.0,(double)nRange,-1.0,1.0);
      glMatrixMode(GL_MODELVIEW);
      glLoadIdentity();
+	 gluLookAt(0.0,0.0,11.0,0.0,0.0,0.0,0.0,1.0,0.0);//3D新增
+
 
 }
 
@@ -107,9 +122,11 @@ void mouse(int button,int state,int x,int y){
 {
 	printf("请输入地图规模n 规模为n*n:\n");
 	scanf("%d",&nRange);
+	//nRange=5;
 	printf("请输入起点和终点编号:\n");
 	scanf("%d %d",&nStartPoint,&nEndPoint);
-
+	//nStartPoint=0;
+	//nEndPoint=24;
 	fill(G[0],G[0]+MAXV*MAXV,INF);
 	init(nRange);	
 	Obstacle(nRange,nStartPoint,nEndPoint);
@@ -121,7 +138,7 @@ void mouse(int button,int state,int x,int y){
 	//--------------
      glutInit(&argc,argv);
      glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGB);
-     glutInitWindowSize(500,500);
+     glutInitWindowSize(700,700);
      glutInitWindowPosition(100,100);
      glutCreateWindow(argv[0]);
      init();
